d81b677506bfe2227cd505ca8e15a6ff6d5e58b7
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-08-02 22:39:34 -0500

    chore: Run tests for extract and transform stages

--- sql/global_amount_order_status.sql
+++ sql/global_amount_order_status.sql
@@ -6,7 +6,7 @@
 -- 3. Group the results by the order status.
 SELECT
     oo.order_status,
-    COUNT(oo.order_status) AS Ammount
+    COUNT(oo.order_status) AS Amount
 FROM
     olist_orders oo
 GROUP BY
